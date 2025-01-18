fbd56e2f1dc66b0f3788e85a1a7fad8db6c5e41a
cgvoller <coreyvoller@hotmail.co.uk>
2025-01-18 21:04:32 +0000

    Update search

--- main.R
+++ main.R
@@ -13,7 +13,9 @@
 ##
 ## ─────────────────────────────────────────────────────────────────────────────
 ##
-## Notes:
+## Notes: Bayesian AND (response-adaptive randomisation/randomization OR 
+##        response adaptive randomisation/randomization)
+## Bayesian AND (adaptive randomization OR response-adaptive randomization OR adaptive allocation)
 ##   
 ##
 ## ─────────────────────────────────────────────────────────────────────────────
@@ -30,44 +32,47 @@ file.list = list.files(path = data_path,pattern="\\.csv$",full.names = T)
 # Create names
 file.names <- gsub(data_path,"",file.list) %>%
   gsub("/dbo_","",.) %>%
-  gsub("\\.csv","",.)
+  gsub("\\.csv","",.) %>% 
+  gsub("/", "", .) %>%
+  gsub("-", "_", .)
 
 # Read in data
 df.list <-lapply(file.list,read.csv)
 
 # Set names
 names(df.list) <- file.names
+# get dfs
+list2env(df.list, envir = .GlobalEnv)
 
-# Clean names
-df.list <- setNames(df.list, tolower(file.names))
-tidy_names <- function(y) {
-  names(y) <- names(y) %>%
-    gsub("/", "", .) %>%
-    gsub("-", "_", .)
-  # Return
-  return(y)
-}
-dfList <- tidy_names(df.list[1])
-#dfList <- lapply(df.list,\(x) {tidy_names(x);x})
-list2env(dfList, envir = .GlobalEnv)
+# Comparing previous search with updated (see notes)
+BRAR_ctg_studies$NCT.Number %in% BRAR2_ctg_studies$NCT.Number
 
-
+brar_ctg_studies <- BRAR2_ctg_studies
+brar_ctg_studies |> dim()
 # Data manipulation ------------------------------------------------------------
 # Change column names
 names(brar_ctg_studies) <- tolower(names(brar_ctg_studies))
 names(brar_ctg_studies) <- gsub(pattern = "\\.","_",names(brar_ctg_studies))
 # Select relevant columns
 studies <- brar_ctg_studies %>% 
-  select(study_status,start_date,completion_date,nct_number,enrollment)
+  select(acronym,study_status,start_date,completion_date,nct_number,enrollment)
+# Turn blanks into NA, where there is an acronym for the trial, use that
+studies %<>% 
+  mutate_if(is.character, list(~na_if(.,"")))  %>% 
+  mutate(nct_number = ifelse(!is.na(acronym),acronym,nct_number),
+         completion_date = ifelse(is.na(completion_date),start_date,completion_date))
 # Fix mixing formats of date
 studies$start_date <- as.Date(parse_date_time(studies$start_date, c("Ym", "Ymd")))
 studies$completion_date <-as.Date(parse_date_time(studies$completion_date, c("Ym", "Ymd")))
 
 
+
 # Plot -------------------------------------------------------------------------
 brar_plot <- studies %>%
+  arrange(start_date) %>% 
   mutate(study_status = tolower(gsub("_", " ", study_status)),
-         study_status=gsub("\\b([a-z])", "\\U\\1", study_status, perl=TRUE)) %>%
+         study_status=gsub("\\b([a-z])", "\\U\\1", study_status, perl=TRUE),
+         nct_number=factor(nct_number,levels = unique(nct_number),ordered = T)) %>%
   ggplot(
     aes(
       x = start_date,
@@ -80,9 +85,11 @@ brar_plot <- studies %>%
   ) +
   geom_segment(size = 0.8) +
   geom_point() +
+  labs(size="Enrollment", colour="Study Status")+
+  #labs(color='Enrollment')+
   scale_x_date(date_breaks = "1 year") +
   xlab('Date') +
-  ylab('NCT Number') +
+  ylab('Trial Name') +
   geom_vline(
     xintercept = Sys.Date(),
     linetype = "dotted",
@@ -92,6 +99,7 @@ brar_plot <- studies %>%
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5))+
   ggtitle("Clinical Trials using Bayesian Response-Adaptive Randomisation")
 
+brar_plot
 width <- 15
 height <- 10
 ### Save plot ------------------------------------------------------------------
